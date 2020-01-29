b39d0a0e322dcef6515888983dfa2e8f3678424b
jesus_isao <jesusisao@gmail.com>
2020-01-29 23:05:46 +0900

    テスト文字列生成ページを作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
 import 'normalize.css';
 import './App.scss';
-import Menu from './menu/Menu'
+import Menu from './Menu/Menu'
+import StringGenerator from './StringGenerator/StringGenerator'
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <Menu></Menu>
+      <div className="App-container">
+        <StringGenerator />
+      </div>
     </div>
   );
 }
---
+++ src/StringGenerator/StringGenerator.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './StringGenerator.scss';
+
+const StringGenerator: React.FC = () => {
+  return (
+    <div className="StringGenerator">
+      <h1>テスト文字列生成</h1>
+      <textarea />
+    </div>
+  );
+}
+
+export default StringGenerator;
