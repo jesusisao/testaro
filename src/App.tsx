a78873485a018a8a2bafef6aa6eeae900955a2c4
isaya.yokoyama <isaya.yokoyama@linkers.net>
2020-01-30 20:43:55 +0900

    ルーティング機能を追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import 'normalize.css';
 import './App.scss';
-import Menu from './Menu/Menu'
-import StringGenerator from './StringGenerator/StringGenerator'
+import Menu from './Menu/Menu';
+import StringGenerator from './StringGenerator/StringGenerator';
+import ImageGenerator from './ImageGenerator/ImageGenerator';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <Menu></Menu>
-      <div className="App-container">
-        <StringGenerator />
+    <BrowserRouter>
+      <div className="App">
+        <Menu></Menu>
+        <div className="App-container">
+          <Route exact path='/' component={StringGenerator} />
+          <Route path='/imggen' component={ImageGenerator} />
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
--- src/Menu/Menu.tsx
+++ src/Menu/Menu.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import './Menu.scss';
+import { Link } from 'react-router-dom';
 
 const Menu: React.FC = () => {
   return (
     <div className="Menu">
       <h1>Testaro.</h1>
       <ul>
-        <li>テスト文字列</li>
-        <li>テストデータ</li>
-        <li>テスト画像</li>
-        <li>Hash</li>
-        <li>バーコード</li>
+        <li><Link to='/'>テスト文字列</Link></li>
+        {/* <li>テストデータ</li> */}
+        <li><Link to='/imggen'>テスト画像</Link></li>
+        {/* <li>Hash</li> */}
+        {/* <li>バーコード</li> */}
       </ul>
     </div>
   );
