b9544970a19f7f6bb8d2d25f8148b836e0e81119
jesus_isao <jesusisao@gmail.com>
2020-02-01 18:16:21 +0900

    fix test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
 
 test("renders learn react link", () => {
-  // const { getByText } = render(<App />);
-  // const linkElement = getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
+  const { getByText } = render(<App />);
+  const title = getByText(/Testaro/i);
+  expect(title).toBeInTheDocument();
 });
