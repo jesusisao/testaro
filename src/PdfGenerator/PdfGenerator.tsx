05e3f30c4025d8c9caee09b5de89b347e460f4f4
jesus_isao <jesusisao@gmail.com>
2020-02-08 09:54:20 +0900

    eslint

--- src/PdfGenerator/PdfGenerator.tsx
+++ src/PdfGenerator/PdfGenerator.tsx
@@ -13,24 +13,24 @@ const PdfGenerator: React.FC = () => {
   const [genNum, setGenNum] = useState(1);
   const [downloading, setDownloading] = useState(false);
 
+  const generatePdf = (num: number): void => {
+    const createdPdfContent = replaceVariable(pdfContent, num);
+    const docDefinition = {
+      content: [{ text: createdPdfContent, fontSize: 48, alignment: "center" }]
+    };
+    const createdFileName = replaceVariable(fileName, num);
+    pdfMake.createPdf(docDefinition).download(createdFileName);
+  };
+
   const generate = async (): Promise<void> => {
     setDownloading(true);
     for (let i = 0; i < genNum; i++) {
-      generatePdf(i + 1)
+      generatePdf(i + 1);
       // あんまり速く大量にDLさせられない
       await sleep(300);
     }
     setDownloading(false);
   };
-
-  const generatePdf = (num: number) => {
-    const createdPdfContent = replaceVariable(pdfContent, num);
-    const docDefinition = {
-      content: [{ text: createdPdfContent, fontSize: 48, alignment: "center" }]
-    };
-    const createdFileName = replaceVariable(fileName, num)
-    pdfMake.createPdf(docDefinition).download(createdFileName);
-  }
 
   return (
     <div className="PdfGenerator">
@@ -61,7 +61,11 @@ const PdfGenerator: React.FC = () => {
           ></input>
         </ParamBox>
         <ParamBox>
-          <button className="testaro-button" disabled={downloading} onClick={generate}>
+          <button
+            className="testaro-button"
+            disabled={downloading}
+            onClick={generate}
+          >
             生成
           </button>
         </ParamBox>
