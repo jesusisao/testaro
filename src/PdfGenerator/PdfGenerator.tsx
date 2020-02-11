5fd7c80ee0828568c7abaad2218c1f7797790a08
jesus_isao <jesusisao@gmail.com>
2020-02-11 10:47:25 +0900

    Robotoを除いて軽量化

--- src/PdfGenerator/PdfGenerator.tsx
+++ src/PdfGenerator/PdfGenerator.tsx
@@ -8,12 +8,6 @@ import pdfMake from "pdfmake/build/pdfmake";
 import vfs from "./lib/vfs_fonts";
 pdfMake.vfs = vfs;
 pdfMake.fonts = {
-  Roboto: {
-    normal: "Roboto-Regular.ttf",
-    bold: "Roboto-Medium.ttf",
-    italics: "Roboto-Italic.ttf",
-    bolditalics: "Roboto-MediumItalic.ttf"
-  },
   KosugiMaru: {
     normal: "KosugiMaru-Regular.ttf",
     bold: "KosugiMaru-Regular.ttf",
