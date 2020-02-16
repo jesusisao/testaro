e3984dac3cbb427b5706b6180ba5d6583eb59685
jesus_isao <jesusisao@gmail.com>
2020-02-16 11:53:42 +0900

    パラメータのlabelのデザイン変更

--- src/Common/ParamBox.tsx
+++ src/Common/ParamBox.tsx
@@ -10,9 +10,14 @@ const style = {
 const labelStyle = {
   display: "inline-block",
   width: "120px",
+  lineHeight: "1.8em",
   textAlign: "right" as "right",
   paddingRight: "5px",
-  verticalAlign: "middle" as "middle"
+  verticalAlign: "middle" as "middle",
+  backgroundColor: "rgba(255, 255, 255, 0.3)",
+  color: "rgb(20, 20, 20)",
+  marginRight: "3px",
+  borderRadius: "5px"
 };
 
 const formItemStyle = {
