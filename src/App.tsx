d60d0f9068b0593c6aac71dddc9e0d40d6d7963c
jesus_isao <jesusisao@gmail.com>
2020-10-18 18:15:02 +0900

    Suspenseの書き方変更

--- src/App.tsx
+++ src/App.tsx
@@ -12,23 +12,6 @@ import Loading from "./Common/Loading";
 const LazyNameGenerator = lazy(() => import("./NameGenerator/NameGenerator"));
 const LazyPdfGenerator = lazy(() => import("./PdfGenerator/PdfGenerator"));
 
-// でかいコンポーネントは遅延で読み込む。
-const LazyPdfGeneratorSuspense = (): JSX.Element => {
-  return (
-    <Suspense fallback={<Loading />}>
-      <LazyPdfGenerator />
-    </Suspense>
-  );
-};
-
-const LazyNameGeneratorSuspense = (): JSX.Element => {
-  return (
-    <Suspense fallback={<Loading />}>
-      <LazyNameGenerator />
-    </Suspense>
-  );
-};
-
 const App: React.FC = () => {
   return (
     // BrowserRouterだとサーバーにリクエストが送られて画面更新時に404になる
@@ -36,14 +19,16 @@ const App: React.FC = () => {
       <div className="App">
         <Menu></Menu>
         <div className="App-container">
-          <Route exact path="/" component={About} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/strgen" component={StringGenerator} />
-          <Route path="/namegen" component={LazyNameGeneratorSuspense} />
-          <Route path="/imggen" component={ImageGenerator} />
-          <Route path="/pdfgen" component={LazyPdfGeneratorSuspense} />
-          <Route path="/pptxgen" component={PptxGenerator} />
-          <Route path="/qrgen" component={QrGenerator} />
+          <Suspense fallback={<Loading />}>
+            <Route exact path="/" component={About} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/strgen" component={StringGenerator} />
+            <Route path="/namegen" component={LazyNameGenerator} />
+            <Route path="/imggen" component={ImageGenerator} />
+            <Route path="/pdfgen" component={LazyPdfGenerator} />
+            <Route path="/pptxgen" component={PptxGenerator} />
+            <Route path="/qrgen" component={QrGenerator} />
+          </Suspense>
 
           {/* 開発用 */}
           <Route path="/dev-load" component={Loading} />
