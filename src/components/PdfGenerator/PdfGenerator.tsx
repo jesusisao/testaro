8aa317dd854b88aab6f060ade525c51b883e7280
jesus_isao <jesusisao@gmail.com>
2020-10-25 20:53:47 +0900

    重いcomponentをlazyに変更

---
+++ src/components/PdfGenerator/PdfGenerator.tsx
@@ -0,0 +1,99 @@
+import React, { useState } from "react";
+import style from "./PdfGenerator.module.scss";
+import commonStyle from "styles/common.module.scss";
+import { sleep, replaceVariable } from "src/components/Common/util";
+import ParamBox from "src/components/Common/ParamBox";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFileDownload } from "@fortawesome/free-solid-svg-icons";
+// import pdfFonts from "pdfmake/build/vfs_fonts";
+import vfs from "../../lib/vfs_fonts";
+import pdfMake from "pdfmake/build/pdfmake";
+import { TDocumentDefinitions } from "pdfmake/interfaces";
+
+pdfMake.vfs = vfs;
+pdfMake.fonts = {
+  KosugiMaru: {
+    normal: "KosugiMaru-Regular.ttf",
+    bold: "KosugiMaru-Regular.ttf",
+    italics: "KosugiMaru-Regular.ttf",
+    bolditalics: "KosugiMaru-Regular.ttf",
+  },
+};
+
+const PdfGenerator: React.FC = () => {
+  const [pdfContent, setPdfContent] = useState("Dummy PDF #{count}");
+  const [fileName, setFileName] = useState("dummy_#{count}");
+  const [genNum, setGenNum] = useState(1);
+  const [downloading, setDownloading] = useState(false);
+
+  const generatePdf = (num: number): void => {
+    const createdPdfContent = replaceVariable(pdfContent, num);
+    const docDefinition: TDocumentDefinitions = {
+      content: [{ text: createdPdfContent, fontSize: 48, alignment: "center" }],
+      defaultStyle: {
+        font: "KosugiMaru",
+      },
+    };
+    const createdFileName = `${replaceVariable(fileName, num)}.pdf`;
+    pdfMake.createPdf(docDefinition).download(createdFileName);
+  };
+
+  const generate = async (): Promise<void> => {
+    setDownloading(true);
+    for (let i = 0; i < genNum; i++) {
+      generatePdf(i + 1);
+      // あんまり速く大量にDLさせられない
+      await sleep(300);
+    }
+    setDownloading(false);
+  };
+
+  return (
+    <div className={style.page}>
+      <h1 className={commonStyle.pageTitle}>ダミーPDF生成</h1>
+      <p>ダミーのPDFファイルをたくさん生成できます。</p>
+      <div className={commonStyle.paramsContainer}>
+        <div className={commonStyle.paramContainer}>
+          <ParamBox labelName="中身の文字">
+            <input
+              type="text"
+              defaultValue={pdfContent}
+              disabled={downloading}
+              onChange={(e): void => setPdfContent(e.target.value)}
+            ></input>
+          </ParamBox>
+          <ParamBox labelName="ファイル名">
+            <input
+              type="text"
+              defaultValue={fileName}
+              disabled={downloading}
+              onChange={(e): void => setFileName(e.target.value)}
+            ></input>
+          </ParamBox>
+          <ParamBox labelName="出力枚数">
+            <input
+              type="number"
+              defaultValue={genNum}
+              disabled={downloading}
+              onChange={(e): void => setGenNum(parseInt(e.target.value))}
+            ></input>
+          </ParamBox>
+
+          <button
+            className={commonStyle.testaroButton}
+            disabled={downloading}
+            onClick={generate}
+          >
+            生成してダウンロード
+            <FontAwesomeIcon
+              icon={faFileDownload}
+              className={commonStyle.icon}
+            />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PdfGenerator;
--- src/pages/user_generator/index.tsx
+++ src/pages/user_generator/index.tsx
@@ -1,87 +1,14 @@
-import React, { useState } from "react";
-import style from "./index.module.scss";
-import commonStyle from "styles/common.module.scss";
-import ParamBox from "src/components/Common/ParamBox";
-import { downloadAsCsv } from "src/components/Common/util";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFileDownload } from "@fortawesome/free-solid-svg-icons";
-import { createUsers, userToCsvText } from "src/models/user/user";
-import UserTable from "src/components/UserGenerator/UserTable";
+import React, { Suspense, lazy } from "react";
+import Loading from "src/components/Common/Loading";
+const LazyComponent = lazy(
+  () => import("src/components/UserGenerator/UserGenerator")
+);
 
 const UserGenerator: React.FC = () => {
-  const [genNum, setGenNum] = useState(20);
-  const [useNumro, setUseNumro] = useState(false);
-  const [mailDomain, setMailDomain] = useState("testaro.netlify.app");
-
-  const generate = (): void => {
-    setGenNum(0);
-    setTimeout(() => {
-      setGenNum(genNum);
-    }, 0);
-  };
-
-  const users = createUsers(genNum, useNumro, mailDomain);
-
-  const downloadCsv = (): void => {
-    downloadAsCsv(userToCsvText(users), "users");
-  };
-
   return (
-    <div className={style.page}>
-      <h1 className={commonStyle.pageTitle}>ダミーユーザー情報生成</h1>
-      <p>
-        セルをクリックすると、中の値が勝手にクリップボードにコピーされます。
-      </p>
-      <div className={commonStyle.paramsContainer}>
-        <div className={commonStyle.paramContainer}>
-          <ParamBox labelName="一条太郎方式">
-            <input
-              type="checkbox"
-              defaultChecked={useNumro}
-              onChange={(e): void => setUseNumro(e.target.checked)}
-            ></input>
-          </ParamBox>
-          <ParamBox labelName="メールのドメイン">
-            <input
-              type="text"
-              defaultValue={mailDomain}
-              onChange={(e): void => setMailDomain(e.target.value)}
-            ></input>
-          </ParamBox>
-          <ParamBox labelName="生成数">
-            <input
-              className={commonStyle.textRight}
-              type="number"
-              defaultValue={genNum}
-              onChange={(e): void => setGenNum(parseInt(e.target.value))}
-            ></input>
-          </ParamBox>
-
-          <button className={commonStyle.testaroButton} onClick={generate}>
-            再生成
-          </button>
-          <button
-            className={commonStyle.testaroButton}
-            onClick={downloadCsv}
-            style={{ marginLeft: 5 }}
-          >
-            CSVダウンロード
-            <FontAwesomeIcon
-              icon={faFileDownload}
-              className={commonStyle.icon}
-            />
-          </button>
-        </div>
-      </div>
-      <div
-        className={commonStyle.outputsContainer}
-        style={{ display: "block" }}
-      >
-        <div className={commonStyle.outputContainer}>
-          <UserTable users={users} />
-        </div>
-      </div>
-    </div>
+    <Suspense fallback={<Loading />}>
+      <LazyComponent />
+    </Suspense>
   );
 };
 
