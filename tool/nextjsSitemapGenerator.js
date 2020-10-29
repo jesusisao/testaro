c2389ea7c508e3eae4dafecf6bb3b60d001e08a2
jesus_isao <jesusisao@gmail.com>
2020-10-29 22:07:27 +0900

    有効なsitemapとなるように修正

--- tool/nextjsSitemapGenerator.js
+++ tool/nextjsSitemapGenerator.js
@@ -1,13 +1,28 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const sitemap = require("nextjs-sitemap-generator");
 const path = require("path");
+const fs = require("fs");
 
-sitemap({
-  baseUrl: "https://testaro.somewhere.gq",
-  pagesDirectory: path.resolve(__dirname + "/../src/pages"),
-  targetDirectory: "out/",
-  sitemapFilename: "sitemap.xml",
-  ignoredExtensions: ["png", "jpg", "scss"],
-});
+const generateSitemap = async () => {
+  await sitemap({
+    baseUrl: "https://testaro.somewhere.gq",
+    pagesDirectory: path.resolve(__dirname + "/../src/pages"),
+    targetDirectory: "out/",
+    sitemapFilename: "sitemap.xml",
+    excludeIndex: "true",
+    ignoredExtensions: ["png", "jpg", "scss"],
+  });
+
+  // プロジェクトの構造上不要な/indexがついてしまうので除去する
+  const filePath = "./out/sitemap.xml";
+  const text = fs.readFileSync(filePath, "utf-8");
+  const newText = text.toString().replace(/\/index/g, "");
+
+  fs.writeFile(filePath, newText, (err) => {
+    if (err) throw err;
+  });
+};
+
+generateSitemap();
 
 console.log(`✅ sitemap.xml generated!`);
