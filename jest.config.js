396ee470af2073393fe2cc1856cc28b8cc1b6bdd
jesus_isao <jesusisao@gmail.com>
2025-03-21 23:11:24 +0900

    upgrade jest (#120)

--- jest.config.js
+++ jest.config.js
@@ -4,20 +4,16 @@ module.exports = {
   roots: ["<rootDir>/src"],
   testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
   transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
+    "^.+\\.(ts|tsx)$": [
+      "ts-jest",
+      {
+        tsconfig: "<rootDir>/test/tsconfig.jest.json",
+      },
+    ],
   },
   moduleNameMapper: {
     "src(.*)$": "<rootDir>/src/$1",
     "\\.(css|scss)$": "identity-obj-proxy",
   },
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  // https://github.com/zeit/next.js/issues/8663#issue-490553899
-  globals: {
-    // we must specify a custom tsconfig for tests because we need the typescript transform
-    // to transform jsx into js rather than leaving it jsx such as the next build requires. you
-    // can see this setting in tsconfig.jest.json -> "jsx": "react"
-    "ts-jest": {
-      tsconfig: "<rootDir>/test/tsconfig.jest.json",
-    },
-  },
 };
