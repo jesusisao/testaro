241f40f7ec23b3c07e25c4b8c708c9f1e0ab2c83
jesus_isao <jesusisao@gmail.com>
2025-03-22 22:30:19 +0900

    コンパイル時に出ていた警告の修正

--- src/components/Common/MetaHeader.tsx
+++ src/components/Common/MetaHeader.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Head from "next/head";
+import Script from "next/script";
 import { GA_ID } from "src/lib/gtag";
 
 const siteName = "Testaro";
@@ -24,45 +25,43 @@ const MetaHeader: React.FC<Prop> = (prop) => {
       : defaultDescription;
   const urlResult = typeof prop.url === "string" ? `${host}${prop.url}` : host;
   return (
-    <Head>
-      <title>{titleResult}</title>
-      <meta name="description" content={descriptionResult} />
-      {/* OGPタグ https://ogp.me/#metadata */}
-      <meta property="og:type" content="website" />
-      <meta property="og:site_name" content={siteName} />
-      <meta property="og:title" content={titleResult} />
-      <meta property="og:description" content={descriptionResult} />
-      <meta property="og:image" content={imageUrl} />
-      <meta property="og:url" content={urlResult} />
-      {/* Twitterカード */}
-      <meta name="twitter:card" content="summary" />
-      <meta name="twitter:site" content="@jesus_isao" />
-      {/* その他 */}
-      <link rel="icon" href="/favicon-32x32.png" />
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Kosugi+Maru&family=M+PLUS+Rounded+1c&display=swap"
-        rel="stylesheet"
-      />
-      <link rel="manifest" href="/manifest.json" />
+    <>
+      <Head>
+        <title>{titleResult}</title>
+        <meta name="description" content={descriptionResult} />
+        {/* OGPタグ https://ogp.me/#metadata */}
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content={siteName} />
+        <meta property="og:title" content={titleResult} />
+        <meta property="og:description" content={descriptionResult} />
+        <meta property="og:image" content={imageUrl} />
+        <meta property="og:url" content={urlResult} />
+        {/* Twitterカード */}
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@jesus_isao" />
+        {/* その他 */}
+        <link rel="icon" href="/favicon-32x32.png" />
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
       {/* Google Analytics */}
-      <script
-        async
+      <Script
+        strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
       />
-      <script
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
-                  window.dataLayer = window.dataLayer || [];
-                  function gtag(){dataLayer.push(arguments);}
-                  gtag('js', new Date());
-                  gtag('config', '${GA_ID}', {
-                    page_path: window.location.pathname,
-                  });`,
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${GA_ID}', {
+                  page_path: window.location.pathname,
+                });`,
         }}
       />
-    </Head>
+    </>
   );
 };
 
