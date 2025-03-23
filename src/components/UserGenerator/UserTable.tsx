20a30a85ada9aed04506ffc92e048fe321d57f2a
jesus_isao <jesusisao@gmail.com>
2025-03-23 14:02:21 +0900

    表示に関するコードはUserからUserTableに寄せる

--- src/components/UserGenerator/UserTable.tsx
+++ src/components/UserGenerator/UserTable.tsx
@@ -1,15 +1,22 @@
+import { DateTime } from "luxon";
 import style from "./UserTable.module.scss";
 import commonStyle from "styles/common.module.scss";
 import { copyToClipboard } from "src/models/util";
-import {
-  User,
-  sortedUserKeys,
-  DisplayUser,
-  usersDisplayHashArray,
-  userDisplayInfo,
-} from "src/models/user/user";
+import { User, sexToJapanese, sexToIso5218 } from "src/models/user/user";
+import { arrayToCsv } from "src/models/util";
 
-export const headerLabels: Record<string, string> = {
+export type DisplayUserKey =
+  | keyof User
+  | "joinedName"
+  | "joinedNameKana"
+  | "joinedNameRome"
+  | "id"
+  | "sexJa"
+  | "sexIso";
+
+export type DisplayUser = Record<DisplayUserKey, string>;
+
+export const headerLabels: DisplayUser = {
   id: "ID",
   joinedName: "氏名",
   familyName: "姓",
@@ -33,6 +40,208 @@ export const headerLabels: Record<string, string> = {
   prefectureKana: "都道府県(カナ)",
   cityKana: "市区町村(カナ)",
   townAreaKana: "町名(カナ)",
+};
+
+type Item = {
+  sortNum: number;
+  width: number;
+  defaultDisplay: boolean;
+};
+
+export const userDisplayInfo: Record<DisplayUserKey, Item> = {
+  id: {
+    sortNum: 1,
+    width: 50,
+    defaultDisplay: true,
+  },
+  joinedName: {
+    sortNum: 2,
+    width: 120,
+    defaultDisplay: true,
+  },
+  familyName: {
+    sortNum: 3,
+    width: 80,
+    defaultDisplay: false,
+  },
+  givenName: {
+    sortNum: 4,
+    width: 80,
+    defaultDisplay: false,
+  },
+  joinedNameKana: {
+    sortNum: 5,
+    width: 120,
+    defaultDisplay: true,
+  },
+  familyNameKana: {
+    sortNum: 6,
+    width: 100,
+    defaultDisplay: false,
+  },
+  givenNameKana: {
+    sortNum: 7,
+    width: 100,
+    defaultDisplay: false,
+  },
+  joinedNameRome: {
+    sortNum: 8,
+    width: 120,
+    defaultDisplay: false,
+  },
+  familyNameRome: {
+    sortNum: 9,
+    width: 80,
+    defaultDisplay: false,
+  },
+  givenNameRome: {
+    sortNum: 10,
+    width: 80,
+    defaultDisplay: false,
+  },
+  sex: {
+    sortNum: 11,
+    width: 60,
+    defaultDisplay: false,
+  },
+  sexJa: {
+    sortNum: 12,
+    width: 60,
+    defaultDisplay: false,
+  },
+  sexIso: {
+    sortNum: 13,
+    width: 100,
+    defaultDisplay: false,
+  },
+  birthday: {
+    sortNum: 14,
+    width: 100,
+    defaultDisplay: true,
+  },
+  email: {
+    sortNum: 15,
+    width: 300,
+    defaultDisplay: true,
+  },
+  postalCode: {
+    sortNum: 16,
+    width: 70,
+    defaultDisplay: true,
+  },
+  prefecture: {
+    sortNum: 17,
+    width: 60,
+    defaultDisplay: true,
+  },
+  city: {
+    sortNum: 18,
+    width: 120,
+    defaultDisplay: true,
+  },
+  townArea: {
+    sortNum: 19,
+    width: 120,
+    defaultDisplay: true,
+  },
+  houseNumber: {
+    sortNum: 20,
+    width: 100,
+    defaultDisplay: true,
+  },
+  prefectureKana: {
+    sortNum: 21,
+    width: 80,
+    defaultDisplay: false,
+  },
+  cityKana: {
+    sortNum: 22,
+    width: 80,
+    defaultDisplay: false,
+  },
+  townAreaKana: {
+    sortNum: 23,
+    width: 80,
+    defaultDisplay: false,
+  },
+};
+
+export const sortedUserKeys: DisplayUserKey[] = (
+  Object.keys(userDisplayInfo) as DisplayUserKey[]
+).sort((a, b) => {
+  if (userDisplayInfo[a]["sortNum"] < userDisplayInfo[b]["sortNum"]) return -1;
+  if (userDisplayInfo[a]["sortNum"] > userDisplayInfo[b]["sortNum"]) return 1;
+  return 0;
+});
+
+export const usersDisplayHashArray = (
+  users: User[],
+  idOffset: number,
+  nameSeparator = " "
+): Array<DisplayUser> => {
+  const items: Array<DisplayUser> = [];
+  for (const [i, user] of users.entries()) {
+    items.push({
+      id: (i + 1 + idOffset).toString(),
+      joinedName: user.familyName + nameSeparator + user.givenName,
+      familyName: user.familyName,
+      givenName: user.givenName,
+      joinedNameKana: user.familyNameKana + nameSeparator + user.givenNameKana,
+      familyNameKana: user.familyNameKana,
+      givenNameKana: user.givenNameKana,
+      joinedNameRome: user.givenNameRome + " " + user.familyNameRome,
+      familyNameRome: user.familyNameRome,
+      givenNameRome: user.givenNameRome,
+      sex: user.sex,
+      sexJa: sexToJapanese(user.sex),
+      sexIso: sexToIso5218(user.sex),
+      birthday: DateTime.fromJSDate(user.birthday).toFormat("yyyy/MM/dd"),
+      email: user.email,
+      postalCode: user.postalCode,
+      prefecture: user.prefecture,
+      city: user.city,
+      townArea: user.townArea,
+      houseNumber: user.houseNumber,
+      prefectureKana: user.prefectureKana,
+      cityKana: user.cityKana,
+      townAreaKana: user.townAreaKana,
+    });
+  }
+  return items;
+};
+
+// 主にCSV用。
+const usersToStringArray = (
+  users: User[],
+  idOffset: number,
+  nameSeparator = " ",
+  displayColumns: Record<DisplayUserKey, boolean>
+): Array<Array<string>> => {
+  // Filter keys based on displayColumns
+  const keysToDisplay = sortedUserKeys.filter((key) => displayColumns[key]);
+  const items: Array<Array<string>> = [keysToDisplay];
+
+  const displayUsers = usersDisplayHashArray(users, idOffset, nameSeparator);
+  for (const user of displayUsers) {
+    const item: Array<string> = [];
+    for (const key of sortedUserKeys) {
+      if (!displayColumns[key]) continue;
+      item.push(user[key]);
+    }
+    items.push(item);
+  }
+  return items;
+};
+
+export const userToCsvText = (
+  users: User[],
+  idOffset: number,
+  nameSeparator = " ",
+  displayColumns: Record<DisplayUserKey, boolean>
+): string => {
+  return arrayToCsv(
+    usersToStringArray(users, idOffset, nameSeparator, displayColumns)
+  );
 };
 
 const generateHeaderRow = (
