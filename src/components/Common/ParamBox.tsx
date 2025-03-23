9998c6488960e7243d5cc9172fe8df07d38f5bad
jesus_isao <jesusisao@gmail.com>
2025-03-23 11:23:10 +0900

    paramboxのCSS切り出し

--- src/components/Common/ParamBox.tsx
+++ src/components/Common/ParamBox.tsx
@@ -1,44 +1,19 @@
 import type { ReactNode } from "react";
-
-const style = {
-  display: "flex",
-  width: "100%",
-  margin: "1px 0 1px 0",
-  lineHeight: "20px",
-  backgroundColor: "rgba(0, 0, 0, 0.5)",
-  boxSizing: "border-box" as const,
-};
-
-const labelStyle = (labelWidth: string | undefined) => {
-  const width = labelWidth ? labelWidth : "120px";
-  return {
-    display: "flex",
-    float: "left" as const,
-    width: width,
-    textAlign: "right" as const,
-    justifyContent: "flex-end",
-    alignItems: "center",
-    padding: "5px",
-    backgroundColor: "rgb(0, 128, 151)",
-    color: "rgba(255, 255, 255, 0.8)",
-  };
-};
-
-const formItemStyle = {
-  display: "flex",
-  float: "right" as const,
-  verticalAlign: "middle" as const,
-};
+import styles from "./ParamBox.module.scss";
 
 const ParamBox: React.FC<{
   labelName?: string;
   labelWidth?: string;
   children: ReactNode;
 }> = (props) => {
+  const labelStyle = props.labelWidth ? { width: props.labelWidth } : {};
+
   return (
-    <div style={style}>
-      <label style={labelStyle(props.labelWidth)}>{props.labelName}</label>
-      <div style={formItemStyle}>{props.children}</div>
+    <div className={styles.container}>
+      <label className={styles.label} style={labelStyle}>
+        {props.labelName}
+      </label>
+      <div className={styles.formItem}>{props.children}</div>
     </div>
   );
 };
