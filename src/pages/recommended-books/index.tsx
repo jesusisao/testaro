603181293a2a4eb57548af52c60489ea7cfa8dd6
jesus_isao <jesusisao@gmail.com>
2025-03-23 19:54:08 +0900

    Amazonアソシエイトのための準備

--- src/pages/recommended-books/index.tsx
+++ src/pages/recommended-books/index.tsx
@@ -6,7 +6,7 @@ import commonStyle from "styles/common.module.scss";
 const RecommendedBooks: NextPage = () => {
   const title = "おすすめ書籍";
   const description =
-    "テスト品質・開発品質を向上させるためのおすすめ書籍です。";
+    "テスト品質・開発品質を向上させるためのおすすめ書籍です。とりあえず一冊持っておいて損はありません。";
   return (
     <div className={style.page}>
       <MetaHeader
@@ -16,35 +16,51 @@ const RecommendedBooks: NextPage = () => {
       />
       <h1 className={commonStyle.pageTitle}>{title}</h1>
       <p>{description}</p>
-      <div className={style.textArea}>
-        <h2>
-          ソフトウェアテストの教科書 品質を決定づけるテスト工程の基本と実践
-        </h2>
-        <p>
-          境界値分析、分岐網羅条件網羅、デシジョンテーブルなど、アプリケーション開発におけるテストについて基本的な手法を知ることができます。
-        </p>
-        <p>
-          この本は基本的にウォーターフォール開発を主眼に置いて内容が書かれているのですが、自動テストを書く上でも、「何をテストしたらよいか」を知っておくことはとても意味があるはずです。
-          <a href="https://www.amazon.co.jp/dp/4797365811/ref=cm_sw_r_tw_dp_x_Z9zJFbGZ46NJC">
-            Amazonで購入する
-          </a>
-        </p>
-      </div>
 
-      <div className={style.textArea}>
-        <h2>テスト駆動開発</h2>
-        <p>
-          「テスト」とタイトルに入ってはいますが、この本はテスト技法の本ではなく開発手法の本です。
-          「テスト駆動開発」とは最初に自動テストを書いて、そのテストが通るようにプロダクションコードを書いていく開発手法です。
-        </p>
-        <p>
-          著者のKent
-          Beckはアジャイルマニフェスト起案者でもあります。エクストリームプログラミングで、どうやって品質を担保していくのか、その基本的な手法を、この本で学ぶことができるかもしれません。
-          <a href="https://www.amazon.co.jp/dp/4274217884/ref=cm_sw_r_tw_dp_x_AbAJFb2Z29XM2">
-            Amazonで購入する
+      <article className={style.article}>
+        <h2 className={style.articleTitle}>ソフトウェアテストの教科書</h2>
+        <div className={style.articleBody}>
+          <a
+            href="https://amzn.to/41UYILO"
+            target="_blank"
+            className={style.image}
+          >
+            <img
+              style={{ width: "160px" }}
+              src="https://m.media-amazon.com/images/I/81pvuoQQr4L._SL1500_.jpg"
+            ></img>
           </a>
-        </p>
-      </div>
+          <p>
+            境界値分析、分岐網羅条件網羅、デシジョンテーブルなど、アプリケーション開発におけるテストについて基本的な手法を知ることができます。
+          </p>
+        </div>
+      </article>
+
+      <article className={style.article}>
+        <h2>テスト駆動開発</h2>
+        <div className={style.articleBody}>
+          <a
+            href="https://amzn.to/4iQgW80"
+            target="_blank"
+            className={style.image}
+          >
+            <img
+              style={{ width: "160px" }}
+              src="https://m.media-amazon.com/images/I/813DXFJioDL._SY425_.jpg"
+            ></img>
+          </a>
+          <span>
+            <p>
+              「テスト」とタイトルに入ってはいますが、この本はテスト技法の本ではなく開発手法の本です。
+              「テスト駆動開発」とは最初に自動テストを書いて、そのテストが通るようにプロダクションコードを書いていく開発手法です。
+            </p>
+            <p>
+              著者のKent
+              Beckはアジャイルマニフェスト起案者でもあります。エクストリームプログラミングで、どうやって品質を担保していくのか、その基本的な手法を、この本で学ぶことができるかもしれません。
+            </p>
+          </span>
+        </div>
+      </article>
     </div>
   );
 };
